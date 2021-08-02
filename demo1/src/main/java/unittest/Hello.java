ebfbd1a11734d592580f4876853e488e505f41b1
Kanyarat Nasomboon <kanyarat.na@kbtg.tech>
2021-08-02 14:01:03 +0700

    success and fail for db

--- demo1/src/main/java/unittest/Hello.java
+++ demo1/src/main/java/unittest/Hello.java
@@ -1,8 +1,21 @@
 package unittest;
 
 public class Hello {
+    UserDB userDB;
 
     public String hi(String name) {
         return "Hello " + name;
+    }
+
+    public String workWithDB(int id) {
+        //Tight coupling => Loose coupling
+        return userDB.getNameById(id);
+    }
+}
+
+class UserDB { // Connect to DB
+    public String getNameById(int id) {
+        //Unstable with dependency
+        throw new RuntimeException("Id=2 not found");
     }
 }
