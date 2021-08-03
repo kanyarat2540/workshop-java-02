68a1b417a12bc511daa4f3cc0c2afc2d8dfbdd25
Kanyarat Nasomboon <kanyarat.na@kbtg.tech>
2021-08-03 14:49:11 +0700

    add worksop

---
+++ src/main/java/com/example/worksopdip/WorksopDipApplication.java
@@ -0,0 +1,13 @@
+package com.example.worksopdip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksopDipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksopDipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worksopdip/WorksopDipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worksopdip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksopDipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
